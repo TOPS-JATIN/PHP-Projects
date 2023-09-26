b23f72d82cb5a831a49aea6a513166f3df5f512b
TOPS-JATIN <140719840+TOPS-JATIN@users.noreply.github.com>
2023-09-26 12:34:49 +0530

    Add files via upload

--- PROJECTS/project_1/list.php
+++ PROJECTS/project_1/list.php
@@ -1,16 +1,23 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>list page</title>
-    <script src="./localstorage.js" defer></script>
+    <!-- <script src="./localstorage.js" defer></script> -->
+    
 </head>
 <body>
     <h1>list page</h1>
-    <button onclick="Show_list()">Show list</button>
+    <button id="Show_List">Show list</button>
      <div id="LIST_Wrapper">
         
     </div> 
 </body>
 </html>
+
+<?php
+
+ print_r($_SESSION["product"]) ;
+?> 
--- PROJECTS/project_1/localstorage.js
+++ PROJECTS/project_1/localstorage.js
@@ -6,32 +6,47 @@ localStorage.getItem("Items") !== null || undefined ? JSON.parse(localStorage.ge
 
 console.log(List_Arrey);
 
+function Save_Local(Arrey) {
+    localStorage.setItem('Items', JSON.stringify(Arrey));
+  
+}
+
+
+ let Add_item_submit = document.getElementById('Add_item_submit');
+  (Add_item_submit !== null )? Add_item_submit.addEventListener('click',Add) : console.log("");
+ Add_item_submit.onclick=Add();
 function Add() {
+    // console.log("Add");
+    // console.log(sessionStorage);
+    
+  
+
     const item_name = document.getElementById('item_name');
     const item_price = document.getElementById('item_price');
     const item_description = document.getElementById('item_description');
     const item_quantity = document.getElementById('item_quantity');
     const item_category = document.getElementById('item_category');
-    const item_image = document.getElementById('item_image');
+     const item_image = document.getElementById('item_image');
 
-  
-    // e.preventDefault();
-    let list={
-        "item_name" : item_name.value,
-        "item_price" : item_price.value,
-        "item_description" : item_description.value,
-        "item_quantity" : item_quantity.value,
-        "item_category" : item_category.value,
-        "item_image" : item_image.value
-    };
+ let list={
+      "item_name" : item_name.value,
+      "item_price" : item_price.value,
+      "item_description" : item_description.value,
+      "item_quantity" : item_quantity.value,
+      "item_category" : item_category.value,
+      "item_image" : item_image.value
+  }; 
    
 
+  List_Arrey.push(list);
+  Save_Local(List_Arrey); 
 
-    List_Arrey.push(list);
-    localStorage.setItem('Items', JSON.stringify(List_Arrey));
-
-    window.location.replace("./list.php");
+    
 };
+
+let Show_List = document.getElementById('Show_List');
+(Show_List !== null )? Show_List.addEventListener('click',Show_list) : console.log("");
+
 
 function Show_list() {
     const LIST_Wrapper = document.getElementById('LIST_Wrapper');
@@ -39,17 +54,94 @@ function Show_list() {
     List_Arrey.forEach((e,i) => {
         console.log(e);
         LIST_Wrapper.innerHTML += `
-            <div class="card" id="${i}">
-                 <div class="card-body">
-                    <h5 class="card-title">${e.item_name}</h5>
-                    <p class="card-text">${e.item_price}</p>
-                    <p class="card-text">${e.item_description}</p>
-                    <p class="card-text">${e.item_quantity}</p>
-                    <p class="card-text">${e.item_category}</p>
-                    <p class="card-text">${e.item_image}</p>
-                    <a href="#" class="btn btn-primary">Edit</a>
-                    <a href="#" class="btn btn-danger">Delete</a>
+            <div class="card" ">
+                 <div class="card-body" id="${i}">
+                    <p  contenteditable="false" class="A_${i}card-text item_name">${e.item_name}</p>
+                    <p  contenteditable="false" class="A_${i}card-text item_price">${e.item_price}</p>
+                    <p  contenteditable="false" class="A_${i}card-text item_description">${e.item_description}</p>
+                    <p  contenteditable="false" class="A_${i}card-text item_quantity">${e.item_quantity}</p>
+                    <p  contenteditable="false" class="A_${i}card-text item_category">${e.item_category}</p>
+                   <!-- <p  contenteditable="false" class="A_${i}card-text item_image">${e.item_image}</p>  -->
+                    <button id="EDIT" class="btn btn-primary" onclick="EDIT(this)">Edit</button>
+                    <button id="DEL" class="btn btn-danger" onclick="DELETE(this)">Delete</button>
                 </div>
             </div>`;
     });
+};
+
+
+
+let NUM
+let PerentNode
+function EDIT(e) {
+    console.log("EDIT");
+    const EDIT_BTN = e;
+     NUM = Number(EDIT_BTN.parentElement.id);
+     PerentNode = document.getElementById(EDIT_BTN.parentElement.id);
+    PerentNode.innerHTML += `
+    <button id="SAVE" class="btn btn-primary" onclick="SAVE(this)">SAVE</button>
+    ` 
+    Add_Remove_ContentEditeble(true);
+};
+
+function Add_Remove_ContentEditeble(Value) {
+    if(Value == false){
+       
+        document.querySelectorAll(`.A_${NUM}card-text`).forEach(element => {
+            element.style.border = "initial";
+            element.setAttribute("contenteditable", `${Value}`);
+            console.log(element.innerText);
+            switch (element.classList[1]) {
+                case "item_name":
+                    List_Arrey[NUM]["item_name"] = element.innerText;
+                    break;
+                case "item_price":
+                    List_Arrey[NUM]["item_price"] = element.innerText;
+                    break;
+                case "item_description":
+                    List_Arrey[NUM]["item_description"] = element.innerText;
+                    break;
+                case "item_quantity":
+                    List_Arrey[NUM]["item_quantity"] = element.innerText;
+                    break;
+                case "item_category":
+                    List_Arrey[NUM]["item_category"] = element.innerText;
+                    break;
+                case "item_image":
+                    List_Arrey[NUM]["item_image"] = element.innerText;
+                    break;
+            
+                default:
+                    break;
+            } 
+
+        });
+
+        console.log(List_Arrey[NUM]);
+        Save_Local(List_Arrey);
+    }else{
+
+        document.querySelectorAll(`.A_${NUM}card-text`).forEach(element => {
+            element.style.border = '2px solid red';
+            element.setAttribute("contenteditable", `${Value}`);
+        });
+    }
 }
+
+function SAVE(e) {
+    console.log(NUM);
+    console.log(PerentNode);
+    e.remove();
+    Add_Remove_ContentEditeble(false);
+
+}
+
+
+
+function DELETE(e) {
+     let perent = e.parentElement;
+     perent.style.display = "none";
+     List_Arrey.splice(0,1);
+     Save_Local(List_Arrey);
+}
+
